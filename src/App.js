02926792a2c2ce37cd191f4f62cca33457dd8da4
stella-loizou <loizou.stella@gmail.com>
2020-04-15 16:14:51 +0100

    Add a container/statefull compoenent called BurgerBuiolder and import it to the App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <p>Test</p>
+        <BurgerBuilder />
       </Layout>
     );
   }
