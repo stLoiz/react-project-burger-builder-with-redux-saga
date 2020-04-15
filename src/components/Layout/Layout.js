cc1e039ba9004d0a825aad0dcb204a8ae52d6188
stella-loizou <loizou.stella@gmail.com>
2020-04-15 16:13:37 +0100

    Add some style to the layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import classes from './Layout.module.css';
+
 const layout = ({ children }) => (
   <>
     <div> Toolbar, SideDrawer, Backdrop</div>
-    <main> {children}</main>
+    <main className={classes.Content}> {children}</main>
   </>
 );
 
