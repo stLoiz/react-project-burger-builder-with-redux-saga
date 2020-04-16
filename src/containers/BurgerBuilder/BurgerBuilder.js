7f835137f72be3e245fb6b24b96e440bbbcdbdad
stella-loizou <loizou.stella@gmail.com>
2020-04-16 08:31:30 +0100

    Import Burger to BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
+import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
   render() {
     return (
       <>
-        <div>Burger</div>
+        <Burger></Burger>
         <div>Build Controls</div>
       </>
     );
