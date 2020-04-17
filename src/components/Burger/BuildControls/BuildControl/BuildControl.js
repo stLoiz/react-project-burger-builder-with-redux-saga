441863f8df0abaad0d02cf610a24783c466d9889
stella-loizou <loizou.stella@gmail.com>
2020-04-17 14:35:12 +0100

    Pass the addIngredientHnadler to the BurgerBuilder, adn execute it there where you have access to the type, instead of passing type and have unecessary extra loop

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import classes from './BuildControl.module.css';
 
-const buildControl = ({ label }) => {
+const buildControl = ({ label, added }) => {
   return (
     <div className={classes.BuildControl}>
       <div className={classes.Labell}>{label}</div>
       <button className={classes.Less}>Less</button>
-      <button className={classes.More}>More</button>
+      <button onClick={added} className={classes.More}>
+        More
+      </button>
     </div>
   );
 };
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -3,12 +3,35 @@ import React from 'react';
 import BuildControl from './BuildControl/BuildControl';
 import classes from './BuildControls.module.css';
 
-const buildControls = ({ controls }) => {
+const controls = [
+  {
+    label: 'Salad',
+    type: 'salad',
+  },
+  {
+    label: 'Bacon',
+    type: 'bacon',
+  },
+  {
+    label: 'Cheese',
+    type: 'cheese',
+  },
+  {
+    label: 'Meat',
+    type: 'meat',
+  },
+];
+
+const buildControls = ({ ingredientAdded }) => {
   return (
-    <div className={classes.buildControls}>
-      {controls.map(control => (
-        <BuildControl key={control.label} label={control.label} />;
-  ))}
+    <div className={classes.BuildControls}>
+      {controls.map((control) => (
+        <BuildControl
+          key={control.label}
+          label={control.label}
+          added={() => ingredientAdded(control.type)}
+        />
+      ))}
     </div>
   );
 };
