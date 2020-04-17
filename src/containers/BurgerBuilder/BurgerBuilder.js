91d3647f60405e0d762e61db8b5228275f3a6085
stella-loizou <loizou.stella@gmail.com>
2020-04-17 14:32:49 +0100

    Add a handler function that will add ingredients and sum the price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7,
+};
+
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
@@ -10,12 +18,25 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0,
     },
+    totalPrice: 4,
   };
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    updatedIngredients[type] = updatedCount;
+    const newPrice = INGREDIENT_PRICES[type] + this.state.totalPrice;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
+
   render() {
     return (
       <>
         <Burger ingredients={this.state.ingredients}></Burger>
-        <BuildControls />
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
       </>
     );
   }
