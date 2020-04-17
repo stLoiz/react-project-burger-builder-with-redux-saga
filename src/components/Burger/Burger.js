076d1e34f81a2a4a0efaf2701c738ce96cf00668
stella-loizou <loizou.stella@gmail.com>
2020-04-17 11:29:28 +0100

    Check if the ingredints arrays is empty
    -In order to do that I use reduce method to flatten the array and be able to check the length of it and see if it is empty.
    Basically what was needed here is to create an array whick wll contain the vallues of each array which is nested in another  array.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,14 +5,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = ({ ingredients }) => {
   // transform object into array in order to use map function
   // Object.keys extracts the keys of an object and turns that into an array
-  const transformIngredients = Object.keys(ingredients).map((igKey) => {
-    // return a new array which will have length equal to the values of the key
-    // return [...Array(ingredients[igKey])];
-    // then map through the array in order to display the amount of the ingredient
-    return [...Array(ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformIngredients = Object.keys(ingredients)
+    .map((igKey) => {
+      // return a new array which will have length equal to the values of the key
+      // return [...Array(ingredients[igKey])];
+      // then map through the array in order to display the amount of the ingredient
+      return [...Array(ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
 
   return (
     <div className={classes.Burger}>
