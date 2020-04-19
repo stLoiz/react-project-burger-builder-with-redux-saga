0e83abd459c63168d15ef7c109fdf19535acfaf9
stella-loizou <loizou.stella@gmail.com>
2020-04-19 21:39:27 +0100

    Create the SideDrawer
    -Adjust the logo on the sideDrawer to be responsive

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import classes from './Layout.module.css';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+
 const layout = ({ children }) => (
   <>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}> {children}</main>
   </>
 );
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import classes from './SideDrawer.module.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+const sideDrawer = () => {
+  return (
+    <div className={classes.SideDrawer}>
+      <div className={classes.Logo}>
+        <Logo />
+      </div>
+
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
+  );
+};
+
+export default sideDrawer;
