1cf71e21415b3b435fdf726ea9ce6ff74cbcccad
stella-loizou <loizou.stella@gmail.com>
2020-04-22 18:42:27 +0100

    Extract the price from url params add pass them to the contactdata as props

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -6,21 +6,23 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      cheese: 1,
-      meat: 1,
-      bacon: 1,
-    },
+    ingredients: null,
+    totalPrice: null,
   };
 
   componentDidMount() {
     const query = new URLSearchParams(this.props.location.search);
     const ingredients = {};
+    let price;
     for (let param of query.entries()) {
+      if (param[0] === 'price') {
+        price = param[1];
+        break;
+      }
       ingredients[param[0]] = +param[1];
     }
-    this.setState({ ingredients: ingredients });
+
+    this.setState({ ingredients: ingredients, totalPrice: price });
   }
 
   checkoutCancelHandler = () => {
@@ -41,7 +43,12 @@ class Checkout extends Component {
         />
         <Route
           path={this.props.match.path + '/contact-data'}
-          render={() => <ContactData ingredients={this.state.ingredients} />}
+          render={() => (
+            <ContactData
+              ingredients={this.state.ingredients}
+              price={this.state.totalPrice}
+            />
+          )}
         />
       </div>
     );
