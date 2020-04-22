dbe1a6e92ac61fd65b01b544bf6ee2e1300498e8
stella-loizou <loizou.stella@gmail.com>
2020-04-22 15:57:27 +0100

    Set the Routes for burgerBuilder and Checkout
    Both routes will be loaded because path='/' is treated as a prefix and when checkout will be hit the burgerBilder route will be hit as well as it has path='/'. To prevent this we can add the exact property to the Route with the prefix'/'  or use the Switch Component to wrap the Routes to only load one of the routes the first one which matches the given path. The order of the routes is important only if we remove the exact word.
    
    I used all the solutions just to show that we can use Switch as well.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -8,8 +9,10 @@ class App extends Component {
   render() {
     return (
       <Layout>
-        <BurgerBuilder />
-        <Checkout />
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/" component={BurgerBuilder} exact />
+        </Switch>
       </Layout>
     );
   }
