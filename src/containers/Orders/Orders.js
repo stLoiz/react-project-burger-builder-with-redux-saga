2c9d67804edc0abca7702e0b97e9a6356edff1b1
stella-loizou <loizou.stella@gmail.com>
2020-04-22 20:11:41 +0100

    Fetch the orders from database

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 
+import axios from '../../axios-orders';
 import Order from '../../components/Order/Order';
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    axios
+      .get('./orders.json')
+      .then((response) => {
+        console.log(response.data);
+        this.setState({ orders: response.data, loading: false });
+      })
+      .catch((error) => {
+        this.setState({ loading: false });
+        console.log(error);
+      });
+  }
   render() {
     return (
       <div>
