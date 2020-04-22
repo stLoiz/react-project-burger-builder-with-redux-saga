7b54f5b10066f5a942decadc2d2d4d323935a995
stella-loizou <loizou.stella@gmail.com>
2020-04-22 19:25:41 +0100

    Pass the props of router to ContactData component
    Because we use the render method  on Route for ContactData we loose the props of the route. One way is to add the props to the render method as an argument and destribute them to the ContactData like this '{..}' in order to be able to access them in ContactData component. Another way is to wrap the export of the ContactData component with the 'withRouter'.

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -43,10 +43,11 @@ class Checkout extends Component {
         />
         <Route
           path={this.props.match.path + '/contact-data'}
-          render={() => (
+          render={(props) => (
             <ContactData
               ingredients={this.state.ingredients}
               price={this.state.totalPrice}
+              {...props}
             />
           )}
         />
