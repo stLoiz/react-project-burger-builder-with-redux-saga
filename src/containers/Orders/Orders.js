98fc33bcea03213263739bafbc4b84b06ed44b3a
stella-loizou <loizou.stella@gmail.com>
2020-04-27 20:32:55 +0100

    Fetch Orders via Redux

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,34 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import axios from '../../axios-orders';
 import Order from '../../components/Order/Order';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import * as actionCreators from '../../store/actions/index';
+import Spinner from '../../components/UI/Spinner/Spinner';
+
 class Orders extends Component {
-  state = {
-    orders: [],
-    loading: true,
-  };
-
   componentDidMount() {
-    axios
-      .get('./orders.json')
-      .then((response) => {
-        const fetchedOrders = [];
-        for (let key in response.data) {
-          fetchedOrders.push({ ...response.data[key], id: key });
-        }
-
-        this.setState({ orders: fetchedOrders, loading: false });
-      })
-      .catch((error) => {
-        this.setState({ loading: false });
-        console.log(error);
-      });
+    this.props.onFetchOrders();
   }
   render() {
     return (
       <div>
-        {this.state.orders.map((order) => {
+        {this.props.loading && <Spinner />}
+        {this.props.orders.map((order) => {
           return (
             <Order
               key={order.id}
@@ -42,4 +29,19 @@ class Orders extends Component {
   }
 }
 
-export default withErrorHandler(Orders, axios);
+const mapStateToProps = (state) => {
+  return {
+    orders: state.order.orders,
+    loading: state.order.loading,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onFetchOrders: () => dispatch(actionCreators.fetchOrders()),
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withErrorHandler(Orders, axios));
