9717c97f4ecf9cb041d05a44f0aeb522882e0438
stella-loizou <loizou.stella@gmail.com>
2020-04-28 13:40:51 +0100

    Create a route for authentication component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import Auth from './containers/Auth/Auth';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
@@ -13,6 +14,7 @@ class App extends Component {
         <Switch>
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={Auth} />
           <Route path="/" component={BurgerBuilder} exact />
         </Switch>
       </Layout>
