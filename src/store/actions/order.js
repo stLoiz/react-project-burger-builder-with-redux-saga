667ec8d2e4ed5f8ca5fde4b69bbb5567745a43a3
stella-loizou <loizou.stella@gmail.com>
2020-04-29 09:24:04 +0100

    Show orders only when the user is authenticated.

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -59,11 +59,11 @@ export const fetchOrdersStart = () => {
     type: actionTypes.FETCH_ORDERS_START,
   };
 };
-export const fetchOrders = () => {
+export const fetchOrders = (token) => {
   return (dispatch) => {
     dispatch(fetchOrdersStart());
     axios
-      .get('/orders.json')
+      .get('/orders.json?auth=' + token)
       .then((response) => {
         const fetchedOrders = [];
         for (let key in response.data) {
