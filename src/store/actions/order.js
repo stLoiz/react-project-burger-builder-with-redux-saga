27b1cac53e84148c01674c2525b5dd731cd0e948
stella-loizou <loizou.stella@gmail.com>
2020-04-29 18:07:40 +0100

    Dispaly user specific orders
    Filter dtabase order by userId

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -59,11 +59,12 @@ export const fetchOrdersStart = () => {
     type: actionTypes.FETCH_ORDERS_START,
   };
 };
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
   return (dispatch) => {
     dispatch(fetchOrdersStart());
+    const queryParams = `?auth=${token}&orderBy="userId"&equalTo="${userId}"`;
     axios
-      .get('/orders.json?auth=' + token)
+      .get(`/orders.json${queryParams}`)
       .then((response) => {
         const fetchedOrders = [];
         for (let key in response.data) {
