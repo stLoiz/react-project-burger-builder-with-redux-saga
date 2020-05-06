810a09bc73b1ae3e5b1be640e526d65aea8e1173
stella-loizou <loizou.stella@gmail.com>
2020-05-06 13:28:10 +0100

    Convert Modal class based component to Functional component
    Use React.memo() to replace shouldComponentUpdate()
    React.memo() allows us to optimize performance and only update the component when the props of the component change. Since previously the logic was to optimize the component and re render it only when props `show` and `children` specifically are changed. W can still do that by having a function as a second argument in React.memo() and define when we want the component to be memorized by comparing the previous props with the nextprops

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,31 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Backdrop from '../Backdrop/Backdrop';
 import classes from './Modal.module.css';
 
-class Modal extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return (
-      nextProps.show !== this.props.show ||
-      nextProps.children !== this.props.children
-    );
-  }
-  render() {
-    return (
-      <>
-        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-        <div
-          className={classes.Modal}
-          style={{
-            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: this.props.show ? '1' : 0,
-          }}
-        >
-          {this.props.children}
-        </div>
-      </>
-    );
-  }
-}
+const Modal = ({ children, show, modalClosed }) => {
+  return (
+    <>
+      <Backdrop show={show} clicked={modalClosed} />
+      <div
+        className={classes.Modal}
+        style={{
+          transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: show ? '1' : 0,
+        }}
+      >
+        {children}
+      </div>
+    </>
+  );
+};
 
-export default Modal;
+export default React.memo(
+  Modal,
+  (prevProps, nextProps) =>
+    nextProps.show === prevProps.show &&
+    nextProps.children === prevProps.children,
+);
