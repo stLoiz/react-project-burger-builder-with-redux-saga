91b8658ef96d3703c427dc95b5ee7ec7ad182767
stella-loizou <loizou.stella@gmail.com>
2020-05-06 13:09:24 +0100

    Convert the Logout class based comnponent to functional component

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
 import * as actionCreators from '../../../store/actions/index';
 
-class Logout extends Component {
-  componentDidMount() {
-    this.props.onLogout();
-  }
+const Logout = ({ onLogout }) => {
+  useEffect(() => {
+    onLogout();
+  }, [onLogout]);
 
-  render() {
-    return <Redirect to="/" />;
-  }
-}
+  return <Redirect to="/" />;
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
