7874a9691ba53b293ded9d1fc4cf3122d9ae4a8e
stella-loizou <loizou.stella@gmail.com>
2020-05-28 12:08:22 +0100

    Use the all function to run mutpile tasks simultaneously

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,4 +1,4 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, all } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
 import {
@@ -15,10 +15,12 @@ export function* watchAuth() {
   // takeEvery is like a listener and will allows us to
   // listen to certain actions like here the action type AUTH_INITIATE_LOGOUT and
   // do something when this action occurs, which is the second argument of takeEvery()
-  yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
-  yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
-  yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
-  yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckSaga);
+  yield all([
+    takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga),
+    takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga),
+    takeEvery(actionTypes.AUTH_USER, authUserSaga),
+    takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckSaga),
+  ]);
 }
 
 export function* watchBurgerBuilder() {
